0a6712cca531b13f7a5bb8346ac072c2fe627d23
zbaisangurov <bvadres@rambler.ru>
2025-01-24 16:07:58 +0200

    JV-15: Подготовлено окружение
    Подтянуты необходимые зависимости, прописана конфигурация базы данных и внешнего сервиса sentry, сконфигурирован logback, обеспечено оформление кода согласно стилю google

--- src/main/java/com/projects/urlcutter/UrlCutterApplication.java
+++ src/main/java/com/projects/urlcutter/UrlCutterApplication.java
@@ -1,13 +1,16 @@
 package com.projects.urlcutter;
 
+import io.sentry.SystemOutLogger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UrlCutterApplication {
+  private static final Logger logger = LoggerFactory.getLogger(UrlCutterApplication.class);
 
-    public static void main(String[] args) {
-        SpringApplication.run(UrlCutterApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(UrlCutterApplication.class, args);
+  }
 }
