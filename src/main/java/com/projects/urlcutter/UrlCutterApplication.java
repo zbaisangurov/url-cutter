f052815ba452022d60dfa37a758aa7b03fdc3610
zbaisangurov <bvadres@rambler.ru>
2025-01-24 14:34:41 +0200

    JV-15: Initial commit

---
+++ src/main/java/com/projects/urlcutter/UrlCutterApplication.java
@@ -0,0 +1,13 @@
+package com.projects.urlcutter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlCutterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlCutterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projects/urlcutter/UrlCutterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.urlcutter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlCutterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
